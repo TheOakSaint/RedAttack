c62b688f6d474e302a2bf4f45bd9509d47031e23
Luís Pires <fc54471@alunos.fc.ul.pt>
2026-04-05 20:59:10 +0100

    basic player and scene

---
+++ scripts/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 130.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var directionH := Input.get_axis("action_left", "action_right")
+	var directionV := Input.get_axis("action_up", "action_down")
+	if directionH:
+		velocity.x = directionH * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if directionV:
+		velocity.y = directionV * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	if velocity.length() > 1:
+		velocity = velocity.normalized() * SPEED
+	
+	move_and_slide()
